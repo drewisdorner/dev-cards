e380ba34d211fbad41cb191355e8d854ecbe9e73
Drewis Dorner <drewis@dorner-it.de>
2020-09-14 20:47:00 +0200

    Updates

---
+++ precache-manifest.07d5f8cf8e6b1f100196a95ccf4d38d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d03d884c6090968496bdcf0454639e3",
+    "url": "/dev-cards/index.html"
+  },
+  {
+    "revision": "287cd9e2d2b65d48113e",
+    "url": "/dev-cards/static/css/main.19319180.chunk.css"
+  },
+  {
+    "revision": "ab561f73427390d624e3",
+    "url": "/dev-cards/static/js/2.a260dfb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dev-cards/static/js/2.a260dfb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287cd9e2d2b65d48113e",
+    "url": "/dev-cards/static/js/main.ba120447.chunk.js"
+  },
+  {
+    "revision": "d90241b6be4d0f979526",
+    "url": "/dev-cards/static/js/runtime-main.f118611a.js"
+  }
+]);
